510877e7563833153c58f497827f8c9b17be59cb
emilynyeo <emily.pcmail@gmail.com>
2023-11-30 14:21:11 -0700

    added manual steps for running smk and doc

--- src/commom.py
+++ src/commom.py
@@ -4,3 +4,9 @@ ssh emye7956@login.rc.colorado.edu
 cd /pl/active/ADOR/projects/mothersmilk/
 # reactivate my github when i log on to the cluster or change nodes
 ssh-add /home/emye7956/.ssh/id_rsa
+
+# run snakemake manually from the main dir
+main_dir="/pl/active/ADOR/projects/mothersmilk/mm_pipeline/"
+out_dir="/pl/active/ADOR/projects/mothersmilk/mm_pipeline/doc/"
+config_dir="/pl/active/ADOR/projects/mothersmilk/mm_pipeline/src/"
+bash $index_script $main_dir $out_dir $config_file 
