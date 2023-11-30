5b583a475027f78dde074c59b2641e64c1405e58
emilynyeo <emily.pcmail@gmail.com>
2023-11-30 12:47:16 -0700

    naming again

---
+++ src/commom.py
@@ -0,0 +1,6 @@
+# log in to the cluster
+ssh emye7956@login.rc.colorado.edu
+# go to MM
+cd /pl/active/ADOR/projects/mothersmilk/
+# reactivate my github when i log on to the cluster or change nodes
+ssh-add /home/emye7956/.ssh/id_rsa
